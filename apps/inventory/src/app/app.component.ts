f752fda37b228f1d6c8822248870e71c44187fdc
magz99 <magzb.99@gmail.com>
2024-11-05 18:06:16 -0500

    Fix imports

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductsComponent } from '@angular-monorepo/products';
 import { UiComponent } from '@angular-monorepo/ui';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, ProductsComponent, UiComponent],
+  imports: [RouterModule, ProductsComponent, UiComponent, NxWelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
